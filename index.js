0332b6743f98f6319277b838e0fd8b91cbecf1a7
Dipto Das <ddipto396@gmail.com>
2023-05-03 03:25:56 +0600

    json data for chef category made

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// const port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
-// const chefInfo = require('./data/chefInfo.json');
+const chefInfo = require('./data/chefInfo.json');
 
 
 
-// app.use(cors());
+app.use(cors());
 
-// app.get('/', (req, res) => {
-//     res.send('Server is running')
-// });
+app.get('/', (req, res) => {
+    res.send('Server is running')
+});
 
-// app.get('/chefinfo', (req, res) => {
-//     res.send(chefInfo);
+app.get('/chefinfo', (req, res) => {
+    res.send(chefInfo);
+})
+
+// app.get('/news/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectedNews = news.find(n => n._id === id);
+//     res.send(selectedNews)
 // })
 
-// // app.get('/news/:id', (req, res) => {
-// //     const id = req.params.id;
-// //     const selectedNews = news.find(n => n._id === id);
-// //     res.send(selectedNews)
-// // })
+// app.get('/categories/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     if (id === 0) {
+//         res.send(news)
+//     }
+//     else {
+//         const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+//         res.send(categoryNews)
+//     }
 
-// // app.get('/categories/:id', (req, res) => {
-// //     const id = parseInt(req.params.id);
-// //     if (id === 0) {
-// //         res.send(news)
-// //     }
-// //     else {
-// //         const categoryNews = news.filter(n => parseInt(n.category_id) === id);
-// //         res.send(categoryNews)
-// //     }
-
-// // })
-
-// app.listen(port, () => {
-//     console.log(`Server API is running on port: ${port}`)
 // })
+
+app.listen(port, () => {
+    console.log(`Server API is running on port: ${port}`)
+})
